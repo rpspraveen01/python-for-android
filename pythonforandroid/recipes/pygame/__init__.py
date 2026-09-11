91e5f6d5b330fda3d9f603f7e38e537e4e2ba7fa
rpspraveen01 <326074795+rpspraveen01@users.noreply.github.com>
2026-09-11 11:09:43 +0530

    pygame: pip-install Cython into hostpython before building compiled components

--- pythonforandroid/recipes/pygame/__init__.py
+++ pythonforandroid/recipes/pygame/__init__.py
@@ -20,6 +20,7 @@ class Pygame2Recipe(CompiledComponentsPythonRecipe):
     name = 'pygame'
 
     depends = ['sdl2', 'sdl2_image', 'sdl2_mixer', 'sdl2_ttf', 'setuptools', 'jpeg', 'png']
+    hostpython_prerequisites = ["setuptools", "Cython<3.2"]
     call_hostpython_via_targetpython = False  # Due to setuptools
     install_in_hostpython = False
 
